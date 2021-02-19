c91e734674841b666e4e6a6ed6842c903ba3a70e
rsdesouza <sdesouza.rodolfo@gmail.com>
2021-02-19 10:20:20 -0300

    Criando cobrinha

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ snake [0] = {
 }
 
 function criarBG() {
-    context.fillSyle = "lightgreen";
-    context.fillReact (0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";
+    context.fillRect (0, 0, 16 * box, 16 * box);
 }
 function criarCobrinha(){
     for (i=0; i < snake.length; i++){
-         
+         context.fillStyle = "green";
+         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
-
 }
