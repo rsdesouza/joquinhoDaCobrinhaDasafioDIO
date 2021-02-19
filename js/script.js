43711d5a8314e05e403e52e102665e83d696f32c
rsdesouza <sdesouza.rodolfo@gmail.com>
2021-02-19 11:09:48 -0300

    Criando o movimento da cobrinha

--- js/script.js
+++ js/script.js
@@ -16,3 +16,26 @@ function criarCobrinha(){
          context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction=="right") snakeX += box;
+    if(direction=="left") snakeX -= box;
+    if(direction=="up") snakeY -= box;
+    if(direction=="down") snakeY += box;
+
+    snake.pop();
+    
+    let newHead = {
+        x:snakeX,
+        y:snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
