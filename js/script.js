2be3d514acd4ca49a97f5a79536ab38b5419c1d9
rsdesouza <sdesouza.rodolfo@gmail.com>
2021-02-19 10:07:16 -0300

    Criando background

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+snake [0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function criarBG() {
+    context.fillSyle = "lightgreen";
+    context.fillReact (0, 0, 16 * box, 16 * box);
+}
+function criarCobrinha(){
+    for (i=0; i < snake.length; i++){
+         
+    }
+
+}
